32acb34c2cfc5d0d3217c20f37e7ba0e061483cc
Hector <h.martinsanchez@H015110.net>
2026-09-08 14:23:52 +0200

    Ariketa 2 eginda

---
+++ 02 - ariketa/script.js
@@ -0,0 +1,10 @@
+//  5 ariketa
+let puntos = 75;
+let rol = (puntos >= 100) ? "VIP" : "Member";
+console.log("5. Ariketa - Rol:", rol);
+
+// 6 ariketa
+let nombre = "Juan";
+let puntosUsuario = 85;
+let rolUsuario = "Member";
+console.log(`Usuario: ${nombre} | Puntos: ${puntosUsuario} | Rol: ${rolUsuario}`);
